c10ebdc501f246117c618a09cc6f6b0d26f4cf03
marionovaro <marionovaro04@gmail.com>
2023-10-19 16:44:46 +0200

    cambios

---
+++ services/pokemon.service.js
@@ -0,0 +1,11 @@
+import { axiosUtil } from "../utils/axiosUtil";
+
+export const getPokemonById = async (id) => {
+  const optionsRequest = {
+    method: "GET",
+    url: `https://pokeapi.co/api/v2/pokemon/${id}`,
+  };
+  return await axiosUtil(optionsRequest);
+
+  //return await axiosUtil(optionsRequest);
+};
