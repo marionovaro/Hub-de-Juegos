367a9deb54ba4c054aab7800871323ac1278a1c9
marionovaro <marionovaro04@gmail.com>
2023-10-19 16:46:21 +0200

    cambios

---
+++ main.js
@@ -0,0 +1,5 @@
+import './style.css';
+import {initController, initTemplate} from "./utils";
+
+initTemplate(); // lanza función de pintar header, footer, y main
+initController(); // lanza función de las direcciones
