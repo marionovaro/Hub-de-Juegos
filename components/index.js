3d29263baef2a64b632c0511156cce94dec0a7d9
marionovaro <marionovaro04@gmail.com>
2023-10-20 17:29:55 +0200

    cambios

--- components/index.js
+++ components/index.js
@@ -2,5 +2,5 @@ export * from "./Header/Header";
 export * from "./Footer/Footer";
 export * from "./Spinner/Spinner";
 export * from "./ButtonFIlter/ButtonFilter";
-export * from "./spinner/SpinnerButton";
+export * from "./Spinner/SpinnerButton";
 export * from "./CardPokemons/CardPokemons"
